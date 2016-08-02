3663711c56375151e5254c9511076080cc5b48d7
ahotchenkov <hotchenkov@list.ru>
2016-08-02 14:03:12 +0300

    mio example is now work

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,5 @@ fn main(){
 
 	chat_server::server_start2();
   
-        // start_server();
-
-        // let mut sock = BufReader::new(TcpStream::connect(HOST).unwrap());
-        // let mut recv = String::new();
-
-        // sock.get_mut().write_all(b"hello world\n").unwrap();
-        // sock.read_line(&mut recv).unwrap();
-
-        // assert_eq!(recv, "hello world\n");
-
-        // recv.clear();
-
-        // sock.get_mut().write_all(b"this is a line\n").unwrap();
-        // sock.read_line(&mut recv).unwrap();
-
-        // assert_eq!(recv, "this is a line\n");
+      
     }
